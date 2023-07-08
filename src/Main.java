0472e3d2341eff56ee32af2de6ec7bd5d3bafdf5
anastasiasokurenko <sokurenkoa1995@gmail.com>
2023-07-08 13:15:00 -0400

    The feature Sun has been added sucessfully

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         Omnax.Planets="2";
         Omnax.Star="4";
 
+        
+        FeatureSun Xeon=new FeatureSun();
+        Xeon.Name="treo";
+        Xeon.size="-34526km";
+        Xeon.temp="-5467373838 celcius";
+
     }
 
 
